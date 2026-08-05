86117ce5dfe19f4850e8c280e44e0a3685e3e201
Rohit Bhure <rohitbhure.cse@gmail.com>
2026-08-05 07:27:44 +0530

    Add transpilePackages to next.config.js for improved package handling

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@
 const nextConfig = {
   typedRoutes: true,
   serverExternalPackages: ["sodium-native", "secure-password"],
+  transpilePackages: ["blitz", "@blitzjs/auth", "@blitzjs/next", "@blitzjs/rpc"],
 }
 
 module.exports = withBlitz(nextConfig)
